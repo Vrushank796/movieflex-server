36976cc83e5710041b0de6f34bb35ca0a96fbbdf
Vrushank796 <42446203+Vrushank796@users.noreply.github.com>
2022-12-09 18:58:24 -0500

    Added CORS header

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors(corsOptions));
 // app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  req.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
