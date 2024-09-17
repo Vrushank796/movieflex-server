da7d4e9bfc4324495d420d31611ee640ec87de16
Vrushank <42446203+Vrushank796@users.noreply.github.com>
2024-09-16 22:08:53 -0400

    Update movieDb.js

--- config/movieDb.js
+++ config/movieDb.js
@@ -6,26 +6,26 @@ const database = process.env.DATABASE;
 
 //Vishal's MongoDB connection url
 
-module.exports = {
-  url:
-    'mongodb+srv://' +
-    username +
-    ':' +
-    password +
-    '@cluster0.dc0fv.mongodb.net/' +
-    database +
-    '?retryWrites=true&w=majority',
-};
-
-//Vrushank's MongoDB connection url
-
 // module.exports = {
 //   url:
 //     'mongodb+srv://' +
 //     username +
 //     ':' +
 //     password +
-//     '@cluster0.ona7y.mongodb.net/' +
+//     '@cluster0.dc0fv.mongodb.net/' +
 //     database +
 //     '?retryWrites=true&w=majority',
 // };
+
+//Vrushank's MongoDB connection url
+
+module.exports = {
+  url:
+    'mongodb+srv://' +
+    username +
+    ':' +
+    password +
+    '@cluster0.ona7y.mongodb.net/' +
+    database +
+    '?retryWrites=true&w=majority',
+};
